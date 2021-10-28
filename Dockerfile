7e69498c389583e981be5cfeb5adb31e2bb7c743
frenkieLee <frenkie.lee@gstech.io>
2021-10-28 02:41:08 +0000

    react + docker 模板
    有基本 line 和 vscode 設定

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,4 @@
 FROM node:14.18
 COPY . .
 COPY id_rsa /root/.ssh/
-RUN npm i
-CMD ["npm", "start"]
 EXPOSE 3000
--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app">
+      <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/app.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="app-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
         </a>
+        <Test>Test</Test>
       </header>
     </div>
   );
 }
 
 export default App;
+
+const Test = styled.div`/*  */
+display: flex;
+justify-content: center;
+align-items: center;
+min-height: 100vh;
+font-size: calc(10px + 2vmin);
+color: white;
+background-color: #282c34;
+flex-direction: column;
+
+.asd {
+  min-height: 100vh;
+  font-size: calc(10px + 2vmin);
+  color: white;
+  background-color: #282c34;
+  flex-direction: column;
+
+  &_sajd {
+    color: white;
+    background-color: #282c34;
+    flex-direction: column;
+  }
+}
+`;
